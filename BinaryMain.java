57cf8293c6993a455d5f2b199cd205feecbd7b39
Vicky Verma <likevishalverma@gmail.com>
2022-12-15 01:44:45 +0530

    Ability to search data in binary search tree

---
+++ BinaryMain.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class BinaryMain {
+
+	public static void main(String[] args) {
+		BinarySearchTree tree = new BinarySearchTree();
+		tree.insert(56);
+		tree.insert(30);
+		tree.insert(70);
+		tree.printData();
+	}
+
+}
---
+++ BinarySearchTree.java
@@ -0,0 +1,38 @@
+package com.bridgelabz;
+
+public class BinarySearchTree {
+	MyNode root;
+
+	public BinarySearchTree() {
+		root = null;
+	}
+
+	public MyNode addRecursive(MyNode current, int data) {
+		if (current == null) {
+			return new MyNode(data);
+		}
+		if (data < current.data) {
+			current.right = addRecursive(current.left, data);
+		} else if (data > current.data) {
+			current.right = addRecursive(current.right, data);
+		} else {
+			return current;
+		}
+		return current;
+	}
+
+	public void insert(int data) {
+		root = addRecursive(root, data);
+	}
+	 public void printData() {
+	        print(root);
+	    }
+	 public void print(MyNode root) {
+	        if (root == null) {
+	            return;
+	        }
+	        print(root.left);
+	        System.out.print(root.data + " ");
+	        print(root.right);
+	    }
+}
